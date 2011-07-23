a3469c521823875feff4c1aa21504476897389c8
Evan Tahler <evantahler@gmail.com>
2011-07-23 13:30:58 -0700

    changing to working directory properly

--- config/initializers/rails_env.rb
+++ config/initializers/rails_env.rb
@@ -1,4 +1,4 @@
 ENV["RAILS_ENV"] = 'synchzor'
 RAILS_ENV = ENV["RAILS_ENV"] unless defined?(RAILS_ENV)
 RAILS_ROOT = File.expand_path(File.dirname(__FILE__) + '/../..') unless defined?(RAILS_ROOT)
-Dir.chdir(RAILS_ENV)
+Dir.chdir(RAILS_ROOT)
